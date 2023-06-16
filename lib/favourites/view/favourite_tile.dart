c6b99e9940abaf2f138dd54859e07572a89a4ef4
hadiyaaamir <hadiyaaamir222@gmail.com>
2023-06-16 11:56:38 +0500

    delete functionality

--- lib/favourites/view/favourite_tile.dart
+++ lib/favourites/view/favourite_tile.dart
@@ -7,6 +7,8 @@ class FavouriteTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // WordController wordController = context.watch<WordController>();
+
     return Padding(
       padding: const EdgeInsets.only(bottom: 5),
       child: Card(
@@ -16,6 +18,16 @@ class FavouriteTile extends StatelessWidget {
           title: Text(
             word.toString(),
             style: Theme.of(context).textTheme.titleMedium,
+          ),
+          trailing: GestureDetector(
+            onTap: () {
+              context.read<WordController>().removeWord(word);
+            },
+            child: Icon(
+              Icons.delete,
+              color: Theme.of(context).colorScheme.primary,
+              size: 20,
+            ),
           ),
         ),
       ),
