467aee4ca9a5d6ae72f4a9370d6cd761308ae39b
hadiyaaamir <hadiyaaamir222@gmail.com>
2023-06-19 15:08:07 +0500

    animated word-list like

--- lib/word_generator/view/small_word_tile.dart
+++ lib/word_generator/view/small_word_tile.dart
@@ -9,7 +9,12 @@ class SmallWordTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: TextButton.icon(
-        icon: word.isFav ? const Icon(Icons.favorite, size: 10) : Container(),
+        icon: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 400),
+          child: word.isFav
+              ? const Icon(key: Key("favourite"), Icons.favorite, size: 10)
+              : Container(key: const Key("not favourite")),
+        ),
         label: Text(word.toString()),
         onPressed: () {
           context.read<WordController>().toggleFavourite(word);
