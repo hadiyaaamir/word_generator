dbf09a9c4c6c480164cf38fa212650816044e667
hadiyaaamir <hadiyaaamir222@gmail.com>
2023-06-19 15:43:47 +0500

    switching animation

--- lib/word_generator/model/word.dart
+++ lib/word_generator/model/word.dart
@@ -5,17 +5,25 @@ class Word {
   const Word({
     required this.word,
     this.isFav = false,
+    this.isSwitched = false,
   });
 
   final WordPair word;
   final bool isFav;
+  final bool isSwitched;
 
-  Word copyWith({WordPair? word, bool? isFav}) =>
-      Word(word: word ?? this.word, isFav: isFav ?? this.isFav);
+  Word copyWith({WordPair? word, bool? isFav, bool? isSwitched}) => Word(
+        word: word ?? this.word,
+        isFav: isFav ?? this.isFav,
+        isSwitched: isSwitched ?? this.isSwitched,
+      );
 
   Word toggleFav() => copyWith(isFav: !isFav);
 
-  Word switchAround() => copyWith(word: WordPair(word.second, word.first));
+  Word switchAround() => copyWith(
+        word: WordPair(word.second, word.first),
+        isSwitched: !isSwitched,
+      );
 
   @override
   String toString() {
